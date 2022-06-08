76d5d8969dd7f173fb1a779eff51c6ab451f2f0a
Sean Oh <ohsean812@gmail.com>
2022-06-08 17:32:25 -0400

    My practice note

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Follow along with the examples here
+/*
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`)
+}
+
+sayHelloTo("Guadalupe");
+sayHelloTo("Liz");
+sayHelloTo("Samip");
+sayHelloTo(1);
+
+function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+}
+
+say("Julio", "hello");
+
+
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+  }
+  console.log(say("Hello", "Liz"))
+
+
+function add(x, y) {
+    return x + y;
+}
+console.log(add(1, 2));
+
+
+function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+  }
+console.log(say("Howdy", "partner"));
+*/
+
+function study(sad, happy) {
+    return `${sad}, but ${happy}!`
+}
+console.log(study("Coding is getting challenging now", "I'm happy to learn"));
